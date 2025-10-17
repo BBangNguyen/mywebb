48dfcc11040c12272d56de55d0b32e5df1cf755f
BBangNguyen <22010952@st.phenikaa-uni.edu.vn>
2025-10-18 02:19:40 +0700

    Connect to Aiven MySQL and add 12 products with images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,54 @@ class DatabaseSeeder extends Seeder
                 'product_image' => './img/product4.png',
                 'product_describe' => 'Điện thoại xịn',
             ],
+            [
+                'product_name' => 'iPhone 15 Pro Max',
+                'product_price' => 29990000,
+                'product_image' => './img/product5.png',
+                'product_describe' => 'iPhone mới nhất với chip A17 Pro',
+            ],
+            [
+                'product_name' => 'Dell XPS 15',
+                'product_price' => 35990000,
+                'product_image' => './img/product6.png',
+                'product_describe' => 'Laptop cao cấp cho dân văn phòng',
+            ],
+            [
+                'product_name' => 'iPad Air M2',
+                'product_price' => 14990000,
+                'product_image' => './img/product7.png',
+                'product_describe' => 'Máy tính bảng mạnh mẽ',
+            ],
+            [
+                'product_name' => 'AirPods Pro Gen 2',
+                'product_price' => 6490000,
+                'product_image' => './img/product8.png',
+                'product_describe' => 'Tai nghe chống ồn tốt nhất',
+            ],
+            [
+                'product_name' => 'Samsung Galaxy S24 Ultra',
+                'product_price' => 31990000,
+                'product_image' => './img/product9.png',
+                'product_describe' => 'Flagship Android mạnh nhất',
+            ],
+            [
+                'product_name' => 'Apple Watch Series 9',
+                'product_price' => 10990000,
+                'product_image' => './img/product10.png',
+                'product_describe' => 'Đồng hồ thông minh cao cấp',
+            ],
+            [
+                'product_name' => 'LG OLED TV 55 inch',
+                'product_price' => 24990000,
+                'product_image' => './img/product11.png',
+                'product_describe' => 'Smart TV màn hình OLED',
+            ],
+            [
+                'product_name' => 'PlayStation 5',
+                'product_price' => 13990000,
+                'product_image' => './img/product12.png',
+                'product_describe' => 'Máy chơi game thế hệ mới',
+            ],
         ]);
     }
 }
